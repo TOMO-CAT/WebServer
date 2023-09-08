56ca7d3e1b05d70c63ee23c9f75558313ff4d7a2
zifeng.yang <zifeng.yang@zelostech.com>
2023-09-08 15:51:45 +0800

    [macros]: unit test

--- util/macros/class_design.h
+++ util/macros/class_design.h
@@ -3,8 +3,3 @@
 #define DISALLOW_COPY_AND_ASSIGN(class_name) \
   class_name(const class_name&) = delete;    \
   void operator=(const class_name&) = delete;
-
-#define DISALLOW_COPY_AND_ASSIGN(class_name) \
- private:                                    \
-  class_name(const class_name&);             \
-  void operator=(const class_name&);
--- util/macros/macros_test.cc
+++ util/macros/macros_test.cc
@@ -7,7 +7,9 @@ namespace macros {
 
 TEST(TestMacros, io_test) {
   PRINT_TO_CONSOLE("int=%d || str=%s || double=%.2f", 101, "cat", 3.1415926);
-  PERROR_TO_CONSOLE("something is wrong!");
+
+  errno = 10;  // No child processes
+  PERROR_TO_CONSOLE("something is wrong");
   EXPECT_EQ(1, 1);
 }
 
